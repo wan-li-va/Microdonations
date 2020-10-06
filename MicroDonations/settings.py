ebcf59f8b7c853e3728a7920d23d01c783996677
Noal Zyglowicz <ntz3sw@virginia.edu>
2020-10-06 03:09:41 -0400

    google auth implemented

--- MicroDonations/settings.py
+++ MicroDonations/settings.py
@@ -39,6 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +128,23 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
--- donations/urls.py
+++ donations/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.urls import include, path
+from django.views.generic import TemplateView  # <--
 
 from . import views
 
 app_name = 'donations'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
 ]
