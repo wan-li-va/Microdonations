685a3750b15e84fa88f5befa613e76cf0043ad09
Noal Zyglowicz <ntz3sw@virginia.edu>
2020-10-09 16:43:46 -0400

    changes restored

--- MicroDonations/settings.py
+++ MicroDonations/settings.py
@@ -134,7 +134,7 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-#SITE_ID = 1
+SITE_ID = 1
 LOGIN_REDIRECT_URL = '/donations'
 
 SOCIALACCOUNT_PROVIDERS = {
