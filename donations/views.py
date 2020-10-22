ce45bce1b69280ca032b2f06f990649115ca2e7c
Noal Zyglowicz <ntz3sw@virginia.edu>
2020-10-21 23:13:51 -0400

    going back to right after merging listpagesfeature into master

--- donations/views.py
+++ donations/views.py
@@ -1,22 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.urls import reverse
 from .models import Organization, Task
+from django.shortcuts import get_object_or_404  # used for shortcut method
+from .models import Profile
+from .forms import ProfileForm
+from .forms import UserForm
 
 # Create your views here.
 
 
 def index(request):
-    template = loader.get_template('donations/index.html')
     context = {}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'donations/index.html', context)
 
 
 def login(request):
-    template = loader.get_template('donations/googlelogin.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
 
 def donations(request):
     list_of_organizations = Organization.objects.all()
@@ -26,6 +29,7 @@ def donations(request):
     }
     return HttpResponse(template.render(context, request))
 
+
 def tasks(request):
     list_of_tasks = Task.objects.all()
     template = loader.get_template('donations/listoftasks.html')
@@ -33,3 +37,32 @@ def tasks(request):
         'list_of_tasks': list_of_tasks,
     }
     return HttpResponse(template.render(context, request))
+    return render(request, 'donations/googlelogin.html', context)
+
+
+def profile(request):
+    profile_bio = request.user.profile.profile_bio
+    profile_phone = request.user.profile.profile_phone
+    profile_location = request.user.profile.profile_location
+    profile_email = request.user.email
+
+    context = {'profile_bio': profile_bio, 'profile_phone': profile_phone,
+               'profile_location': profile_location, 'profile_email': profile_email}
+    return render(request, 'donations/profile.html', context)
+
+
+def edit_profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        form = ProfileForm(request.POST, instance=request.user.profile)
+        if form.is_valid() and user_form.is_valid():
+            user_form.save()
+            form.save()
+            return redirect('/donations/profile')
+    else:
+        user_form = UserForm(instance=request.user)
+        form = ProfileForm(instance=request.user.profile)
+
+    context = {'user_form': user_form,
+               'form': form}
+    return render(request, 'donations/edit_profile.html', context)
