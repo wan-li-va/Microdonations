89f3752b75343050a4651aed7a5d4ac6074430bf
lianapoon <57880588+lianapoon@users.noreply.github.com>
2020-10-23 12:19:52 -0400

    Create functions.py

---
+++ donations/functions/functions.py
@@ -0,0 +1,13 @@
+#basic functions for app
+
+def sortByAlphabetical(unsorted_list):
+    sorted_list = sorted(unsorted_list)
+    return sorted_list
+
+
+def calculateTotal(unsorted_list):
+    total = sum(unsorted_list)
+    return total
+
+def equals(x, y):
+    return (x==y)
