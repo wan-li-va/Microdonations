e614f01a98c9f58c9b896dcb44a03bd230ff806b
lianapoon <57880588+lianapoon@users.noreply.github.com>
2020-11-06 15:57:53 -0500

    Update tests.py

--- donations/tests.py
+++ donations/tests.py
@@ -1,3 +1,32 @@
+# Create your tests here.
+import pytest
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Organization, Task, Profile
+
+class OrganizationTestCase(TestCase):
+    def setUp(self):
+        #Organization.objects.create(organization_text="a", description_text="b")
+        Organization.organization_text = 'organization_text'
+        Organization.description_text = 'description_text'
+
+        Task.task_text = 'task_text'
+        Task.description_text = 'description_text'
+
+        Profile.profile_bio = "profile_bio"
+        Profile.profile_location = "profile_location"
+        Profile.profile_phone = "profile_phone"
+
+    def test_organization_model(self):
+        #organization_text = Organization.objects.get(organization_text="a")
+        self.assertEqual(Organization.organization_text, "organization_text")
+        self.assertEqual(Organization.description_text, 'description_text')
+
+    def test_task_model(self):
+        self.assertEqual(Task.task_text, "task_text")
+        self.assertEqual(Task.description_text, "description_text")
+
+    def test_profile_model(self):
+        self.assertEqual(Profile.profile_bio, "profile_bio")
+        self.assertEqual(Profile.profile_location, "profile_location")
+        self.assertEqual(Profile.profile_phone, "profile_phone")
