e634e9c881d663755b165890f4a58fe048ec65a8
Noal Zyglowicz <ntz3sw@virginia.edu>
2020-11-24 15:16:33 -0500

    citations added and slight html/css work

--- MicroDonations/urls.py
+++ MicroDonations/urls.py
@@ -1,18 +1,15 @@
+# <!-- /***************************************************************************************
+# *  REFERENCES
+# *  Title: In 5 mins: Set up Google login to sign up users on Django
+# *  Author: Zoe Chew
+# *  Date: July 27 2019
+# *  Code version: 1.0
+# *  URL: https://whizzoe.medium.com/in-5-mins-set-up-google-login-to-sign-up-users-on-django-e71d5c38f5d5
+# *  Software License: N/A
+# ***************************************************************************************/ -->
+
 # """MicroDonations URL Configuration
 
-# The `urlpatterns` list routes URLs to views. For more information please see:
-#     https://docs.djangoproject.com/en/3.1/topics/http/urls/
-# Examples:
-# Function views
-#     1. Add an import:  from my_app import views
-#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-# Class-based views
-#     1. Add an import:  from other_app.views import Home
-#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-# Including another URLconf
-#     1. Import the include() function: from django.urls import include, path
-#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-# """
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView  # <--
